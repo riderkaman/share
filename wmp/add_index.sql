bf77cdfce6783baa42b7ee86826a6541da0ca87d
rednics <rednics@naver.com>
2019-03-15 18:51:27 +0900

    Create add_index.sql

---
+++ wmp/add_index.sql
@@ -0,0 +1,3 @@
+create index IX_mPBT_TALK_LOGS_tID_rID_dbRegDate On mPBT_Talk_Logs (roomId, dbregdate);     
+
+create index IX_mPBT_P2P_Room_Member_Info_rID_updDt On mPBT_P2P_Room_Member_Info (roomId, lastUpdateDate);   
