6ab2968ae313fdab7a30798d260de3bc6f817db2
rednics <rednics@naver.com>
2019-03-29 01:08:26 +0900

    -

---
+++ release/ps/shutdown.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+. "./setenv.sh"
+
+if [ -f $SERVER_NAME.pid ] ; then
+	echo "Stop $SERVER_NAME..."
+	kill -9 `cat $SERVER_NAME.pid`
+	rm -f $SERVER_NAME.pid
+else
+	echo "$SERVER_NAME.pid file does not exists!"
+fi
---
+++ release/ps/startup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+. "./setenv.sh"
+
+if [ -f $SERVER_NAME.pid ] ; then
+	kill -9 `cat $SERVER_NAME.pid`
+fi
+
+echo "Start $SERVER_NAME...."
+#nohup java $JAVA_OPTS $DEBUG_OPTS $JMX_OPTS -jar $LAUNCHER $SERVER_NAME > nohup.out &
+nohup java $JAVA_OPTS $GC_OPTS -jar $LAUNCHER $SERVER_NAME > nohup.out &
+echo $! > $SERVER_NAME.pid
+tail -f nohup.out
+
