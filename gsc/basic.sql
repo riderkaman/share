673307fe70358345f98bf8b0c14fb90fb76990d5
rednics <rednics@naver.com>
2019-04-03 14:18:11 +0900

    Update basic.sql

--- gsc/basic.sql
+++ gsc/basic.sql
@@ -3,14 +3,16 @@ SELECT userInfo.userIdnfr,
 	   userInfo.name, 
 	   userInfo.position, 
 	   deviceInfo.userDevceIdnfr, 
-	   deviceInfo.uuid, 
 	   deviceInfo.devceOsType,
-	   pushkeyInfo.pcPushKey
+	   pmsPushkeyInfo.pcPushKey,
+	   ppsPushkeyInfo.UUID
   FROM [MTALK].[pms].[mPMS_USER_INFO10] userInfo,
 		[MTALK].[pms].[mPMS_USER_DEVICE10] deviceInfo,
-		[MTALK].[pms].[mPMS_PUSH_KEY_INFO] pushkeyInfo
+		[MTALK].[pms].[mPMS_PUSH_KEY_INFO] pmsPushkeyInfo,
+		[MTALK].[pps].[mPPS_PUSH_KEY_MAP] ppsPushkeyInfo
   where userinfo.userIdnfr = deviceInfo.userIdnfr
-  and deviceInfo.userDevceIdnfr = pushkeyInfo.userDevceIdnfr
+  and deviceInfo.userDevceIdnfr = pmsPushkeyInfo.userDevceIdnfr
+  and pmsPushKeyInfo.pcPushKey = ppsPushkeyInfo.PPC_PUSH_KEY
   and userInfo.regiStusDstcd = '10'
   and deviceInfo.regiStusDstcd = '10'
   and userInfo.name = '이승원'
