bf549bfc6ada974fda8c2e4bac25991db01c6bc6
rednics <rednics@naver.com>
2019-04-03 14:56:43 +0900

    Update basic.sql

--- gsc/basic.sql
+++ gsc/basic.sql
@@ -1,25 +1,30 @@
 SELECT userInfo.userIdnfr, 
        userInfo.sawonNo, 
-	   userInfo.name, 
-	   userInfo.position, 
-	   deviceInfo.userDevceIdnfr, 
-	   deviceInfo.devceOsType,
-	   pmsPushkeyInfo.pcPushKey,
-	   ppsPushkeyInfo.UUID,
-	   pushHistory.ID,
-	   pushHistory.PPC_PUSH_STATUS
+       userInfo.name, 
+       userInfo.position, 
+       deviceInfo.userDevceIdnfr, 
+       deviceInfo.devceOsType,
+       pmsPushkeyInfo.pcPushKey,
+       ppsPushkeyInfo.UUID,
+       pushHistory.ID,
+       pushHistory.PPC_PUSH_STATUS,
+       pushHistory.REG_DT,
+       pushData.PAYLOAD
   FROM [MTALK].[pms].[mPMS_USER_INFO10] userInfo,
-		[MTALK].[pms].[mPMS_USER_DEVICE10] deviceInfo,
-		[MTALK].[pms].[mPMS_PUSH_KEY_INFO] pmsPushkeyInfo,
-		[MTALK].[pps].[mPPS_PUSH_KEY_MAP] ppsPushkeyInfo,
-	    [MTALK].[pps].[MPPS_PUSH_DEVICE] pushDevice,
-	    [MTALK].[pps].[MPPS_PUSH_DATA_HISTORY] pushHistory
-  where userinfo.userIdnfr = deviceInfo.userIdnfr
-  and deviceInfo.userDevceIdnfr = pmsPushkeyInfo.userDevceIdnfr
-  and pmsPushKeyInfo.pcPushKey = ppsPushkeyInfo.PPC_PUSH_KEY
-  and ppsPushkeyInfo.PPC_PUSH_KEY = pushDevice.PPC_PUSH_KEY
-  and pushDevice.DEVICE_MSG_UUID = pushHistory.DEVICE_MSG_UUID
-  and userInfo.regiStusDstcd = '10'
-  and deviceInfo.regiStusDstcd = '10'
-  and userInfo.name = '이승원'
-  order by pushHistory.reg_dt asc;
+       [MTALK].[pms].[mPMS_USER_DEVICE10] deviceInfo,
+       [MTALK].[pms].[mPMS_PUSH_KEY_INFO] pmsPushkeyInfo,
+       [MTALK].[pps].[mPPS_PUSH_KEY_MAP] ppsPushkeyInfo,
+       [MTALK].[pps].[MPPS_PUSH_DEVICE] pushDevice,
+       [MTALK].[pps].[MPPS_PUSH_DATA_HISTORY] pushHistory,
+       [MTALK].[pps].[MPPS_PUSH_DATA] pushData
+  WHERE userinfo.userIdnfr = deviceInfo.userIdnfr
+  AND deviceInfo.userDevceIdnfr = pmsPushkeyInfo.userDevceIdnfr
+  AND pmsPushKeyInfo.pcPushKey = ppsPushkeyInfo.PPC_PUSH_KEY
+  AND ppsPushkeyInfo.PPC_PUSH_KEY = pushDevice.PPC_PUSH_KEY
+  AND pushDevice.DEVICE_MSG_UUID = pushHistory.DEVICE_MSG_UUID
+  AND pushDevice.MSG_UUID = pushData.MSG_UUID
+  AND userInfo.regiStusDstcd = '10'
+  AND deviceInfo.regiStusDstcd = '10'
+  AND userInfo.name = '이승원'
+  AND deviceInfo.devceOsType in ('A','P')
+  ORDER BY pushHistory.reg_dt DESC;
