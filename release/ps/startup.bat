71cdf49611f7ccd8429361f67ccfc8325d3c6c17
rednics <rednics@naver.com>
2019-04-08 15:07:57 +0900

    -

--- release/ps/startup.bat
+++
@@ -1,8 +0,0 @@
-call setenv.bat
-
-TITLE %SERVER_NAME%
-SETLOCAL enabledelayedexpansion
-chcp 65001
-
-rem "%JAVACMD%" -jar %JAVA_OPTS% %DEBUG_OPTS% %JMX_OPTS% %LAUNCHER%
-"%JAVACMD%" -jar %JAVA_OPTS% %JMX_OPTS% %LAUNCHER% >> console.out
